bf3939a75872ef917d2c25498f567474ec9caf05
HEEBERT-ROSHAN <heebertroshan@gamil.com>
2025-07-26 21:59:23 +0530

    commit message

---
+++ client/src/components/StudentCard.jsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+function StudentCard({ student,  onDelete }) {
+  return (
+    <div className="bg-white shadow-md rounded-xl p-4 text-gray-800">
+      <h2 className="text-xl font-semibold">{student.name}</h2>
+      <p>
+        <strong>Roll:</strong> {student.roll}
+      </p>
+      <p>
+        <strong>Dept:</strong> {student.department}
+      </p>
+      <p>
+        <strong>Email:</strong> {student.email}
+      </p>
+      <div className="mt-4 flex gap-3">
+        <button
+          onClick={() => navigate(`/edit/${student.id}`)}
+          className="bg-yellow-500 text-white px-3 py-1 rounded"
+        >
+          Edit
+        </button>
+        <button
+          onClick={() => onDelete(student.id)}
+          className="bg-red-500 text-white px-3 py-1 rounded"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default StudentCard;
